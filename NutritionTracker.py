3ca41ba7be7258a432a55f9d53a812e463eb4eee
LordRafium <124652285+LordRafium@users.noreply.github.com>
2023-11-12 18:35:20 -0500

    Update

--- NutritionTracker.py
+++ NutritionTracker.py
@@ -2,6 +2,7 @@ from Manager import Manager
 from Food import Food
 from Day import Day
 from datetime import date
+from tkinter import *
 import os
 
 manager = Manager()     
@@ -36,8 +37,95 @@ if day.maxCalories == 0 or day.maxCarbs == 0 or day.maxProtein == 0 or day.maxFa
     print("Welcome to the Macro tracker. To start enter your macro goals/limits")
     day.changeGoal()
 
+#Button Commands------------------
 
+def switchToEatFood():
+    frameMain.pack_forget()
+    frameEatFood.pack()
 
+def switchToAddFood():
+    frameMain.pack_forget()
+    frameAddFood.pack()
 
+def submitAddFood():
+    name = nameEntry.get()
+    cal = caloriesEntry.get()
+    protein = proteinEntry.get()
+    fat = fatEntry.get()
+    carb = carbEntry.get()
+    manager.addFood(name, cal, protein, fat, carb)
 
+def eat():
+    pass
+
+def backFromEat():
+    frameEatFood.pack_forget()
+    frameMain.pack()
+
+def backFromAdd():
+    frameAddFood.pack_forget()
+    frameMain.pack()
+
+#GUI---------------------------------
+
+window = Tk()
+window.geometry("420x420")
+
+frameMain = Frame(window)
+frameAddFood = Frame(window)
+frameEatFood = Frame(window)
+
+frameMain.pack()
+frameAddFood.pack_forget()
+frameEatFood.pack_forget()
+
+#main
+
+eatFoodButton = Button(frameMain,text = 'Eat Food', command = switchToEatFood).pack()
+addFoodButton = Button(frameMain,text = 'Add Food', command = switchToAddFood).pack()
+
+#Eat Food
+roww = 0
+coll = 0
+for food in manager.foods:
+    foodLabel = Label(frameEatFood, text = food.name)
+    foodLabel.grid(row = roww, column = coll)
+    eatButton = Button(frameEatFood, text = 'Eat', command = eat)
+    eatButton.grid(row = roww, column = coll+1)
+    roww = roww + 1
+    
+eatToMain = Button(frameEatFood, text = 'Back', command = backFromEat)
+eatToMain.grid(row = roww+1, column = 0)
+
+#Add Food
+
+nameLabel = Label(frameAddFood, text = 'Name')
+nameLabel.grid(row = 0, column = 0)
+nameEntry = Entry(frameAddFood)
+nameEntry.grid(row = 0, column = 1)
+caloriesLabel = Label(frameAddFood, text = 'Calories')
+caloriesLabel.grid(row = 1, column = 0)
+caloriesEntry = Entry(frameAddFood)
+caloriesEntry.grid(row = 1, column = 1)
+proteinLabel = Label(frameAddFood, text = 'Protein')
+proteinLabel.grid(row = 2, column = 0)
+proteinEntry = Entry(frameAddFood)
+proteinEntry.grid(row = 2, column = 1)
+fatLabel = Label(frameAddFood, text = 'Fat')
+fatLabel.grid(row = 3, column = 0)
+fatEntry = Entry(frameAddFood)
+fatEntry.grid(row = 3, column = 1)
+carbLabel = Label(frameAddFood, text = 'Carbohydrates')
+carbLabel.grid(row = 4, column = 0)
+carbEntry = Entry(frameAddFood)
+carbEntry.grid(row = 4, column = 1)
+
+submit = Button(frameAddFood, text = 'Submit', command = submitAddFood)
+submit.grid(row = 5, column = 1)
+
+addToMain = Button(frameAddFood, text = 'Back', command = backFromAdd)
+addToMain.grid(row = 5, column = 0)
+#---------------------------
+
+window.mainloop()
 
